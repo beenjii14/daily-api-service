1741cd57fca366ba7beac970133e90d4e0b9af42
Benjamin Cruz <benjamin_cruz@Outlook.es>
2022-04-23 02:39:32 -0500

    fix: 🚑 base url in the commands

--- build/index.js
+++ build/index.js
@@ -4,12 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const diaries_1 = __importDefault(require("routes/diaries"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 app.use(express_1.default.json()); // middleware to parse body of request to json
-app.use('/ping', (_req, res) => {
-    res.send('pong').status(200);
-});
+app.use(diaries_1.default);
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
